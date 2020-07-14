db41b96806bf11850e9b2bb51371852001836e1f
hafidzalnur <muhamadhafidz07@gmail.com>
2020-07-14 08:49:57 +0700

    upload project vue

---
+++ src/views/Cart.vue
@@ -0,0 +1,38 @@
+<template>
+
+    <div>
+        <HeaderShayna />
+    
+        <!-- Breadcrumb Section Begin -->
+        <div class="breacrumb-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="breadcrumb-text product-more text-left">
+                            <router-link to="/"><i class="fa fa-home"></i> Home</router-link>
+                            <span>Shopping Cart</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Breadcrumb Section Begin -->
+
+        <CartShayna />
+    </div>
+</template>
+
+<script>
+
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import CartShayna from '@/components/CartShayna.vue'
+
+export default {
+    name: "Cart",
+    components: {
+        HeaderShayna,
+        CartShayna
+    }
+
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroSection />
+    <BannerShayna />
+    <InstaShayna />
+    <PartnerLogos />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroSection from '@/components/HeroSection.vue'
+import BannerShayna from '@/components/BannerShayna.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import PartnerLogos from '@/components/PartnerLogos.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroSection,
+    BannerShayna,
+    InstaShayna,
+    PartnerLogos,
+    FooterShayna
+  }
+}
+</script>
